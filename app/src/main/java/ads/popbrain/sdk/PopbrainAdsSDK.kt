a3f74c2913f7d00abf2d865ffb49c16d9dde32a8
Sandeep Malik <maliksandeep8054@gmail.com>
2026-04-02 16:55:43 +0530

    bug resolved

--- app/src/main/java/ads/popbrain/sdk/PopbrainAdsSDK.kt
+++ app/src/main/java/ads/popbrain/sdk/PopbrainAdsSDK.kt
@@ -6,7 +6,6 @@ import android.util.Log
 object PopbrainAdsSDK {
     fun initialize(context: Context) {
         InstallReferrerManager.fetchReferrer(context) { result ->
-            Log.d("PopbrainSDK", "Conversion detected: ${result.}")
             ConversionTracker.track(result)
         }
     }
