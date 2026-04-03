d8daac13e27b4058ce46aec13a32225d6ba3bcb8
Sandeep Malik <maliksandeep8054@gmail.com>
2026-04-03 11:07:49 +0530

    rolback kiya

--- app/src/main/java/ads/popbrain/sdk/ConversionTracker.kt
+++ app/src/main/java/ads/popbrain/sdk/ConversionTracker.kt
@@ -7,32 +7,9 @@ object ConversionTracker {
 
     fun track(result: AttributionResult) {
         if (!result.isOrganic) {
-            val clickId = extractClickId(result.referrer!!)
-            Log.d("PopbrainSDK", "referrer: ${result.referrer}")
-            if (clickId.isNotEmpty()) {
-                ConversionApi.reportInstall(clickId, result.referrer)
-            }
+            Log.d("PopbrainSDK", "Conversion detected: ${result.referrer}")
         } else {
             Log.d("PopbrainSDK", "Organic install")
         }
-    }
-
-    fun extractClickId(referrer: String): String {
-        try {
-            val params = referrer.split("&")
-            for (param in params) {
-                val pair = param.split("=")
-                if (pair.size == 2) {
-                    val key = pair[0].trim()
-                    val value = pair[1].trim()
-                    if (key.equals("clickId", ignoreCase = true)) {
-                        return value
-                    }
-                }
-            }
-        } catch (e: Exception) {
-            println("PopbrainSDK: Parsing Error: ${e.message}")
-        }
-        return ""
     }
 }
