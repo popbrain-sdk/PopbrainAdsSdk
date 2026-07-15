1f99935cc708fbdabb90727d976bf2056c6bad4c
Sandeep Malik <maliksandeep8054@gmail.com>
2026-07-15 11:01:47 +0530

    Changed encoded way

--- app/src/main/java/ads/popbrain/sdk/ReferrerParser.kt
+++ app/src/main/java/ads/popbrain/sdk/ReferrerParser.kt
@@ -2,25 +2,35 @@ package ads.popbrain.sdk
 
 import ads.popbrain.sdk.AttributionResult
 import android.util.Log
+import java.net.URLDecoder
 
 object ReferrerParser {
 
     fun parse(referrer: String?): AttributionResult {
-//        Log.d("PopbrainSDK", "Raw Referrer: $referrer")
         if (referrer.isNullOrEmpty() || referrer.contains("organic", ignoreCase = true)) {
             return AttributionResult(isOrganic = true)
         }
 
-        val params = referrer.split("&").mapNotNull {
+        val decodedReferrer = try {
+            URLDecoder.decode(referrer, "UTF-8")
+        } catch (e: Exception) {
+            referrer
+        }
+
+        val params = decodedReferrer.split("&").mapNotNull {
             val pair = it.split("=")
-            if (pair.size == 2) pair[0] to pair[1] else null
+            if (pair.size >= 2) {
+                pair[0] to pair.drop(1).joinToString("=")
+            } else null
         }.toMap()
 
         val extractedClickId = params["clickId"] ?: params["utm_clickId"]
 
-        val hasPopbrainSource = params["utm_source"]?.contains("popbrain", ignoreCase = true) == true
-        val isNotOrganic = hasPopbrainSource || !extractedClickId.isNullOrEmpty() || params.containsKey("gclid")
-
+        val hasPopbrainSource =
+            params["utm_source"]?.contains("popbrain", ignoreCase = true) == true
+        val isNotOrganic =
+            hasPopbrainSource || !extractedClickId.isNullOrEmpty() || params.containsKey("gclid")
+        println("PopbrainSDK: Referrer : $referrer")
         return AttributionResult(
             isOrganic = !isNotOrganic,
             referrer = referrer,
